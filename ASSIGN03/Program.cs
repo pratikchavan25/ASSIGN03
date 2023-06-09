0c21d8af3d3976ac3ddc9883c0f0bdf4770f579a
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-09 13:48:46 +0530

    Add project files.

---
+++ ASSIGN03/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//3.Accept two angles of triangle and find third angle.
+
+Console.WriteLine("angle one");
+
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("angle two");
+
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Console.ReadLine());
+
+int c = a + b;
+
+Console.WriteLine(c);
